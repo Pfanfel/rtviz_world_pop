9e4c1bcfeeda5d8486df879f27d9b3598603bdae
Tobias Kutscher <t.kutscher@schig.com>
2024-12-01 12:59:11 +0100

    changed api response to json object instead of list

--- src/backend/fastAPI/main.py
+++ src/backend/fastAPI/main.py
@@ -1,14 +1,48 @@
 from fastapi import FastAPI, Request
+import math
 from pyquadkey2 import quadkey
 import duckdb
+from contextlib import asynccontextmanager
+import json
 
-app = FastAPI(debug=True)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Run at startup
+    Initialise the Client and add it to request.state
+    """
+    # Build the tree
+    con = duckdb.connect("../../data/qudkeyDB.duckdb") # Note: duckdb.sql connects to the default in-memory database connection
+    con.install_extension("spatial")
+    con.load_extension("spatial")
+    yield {"con" : con }
+
+    """ Run on shutdown
+        Close the connection
+        Clear variables and release the resources
+    """
+    # Cleanup the tree?
+
+app = FastAPI(lifespan=lifespan)
 
 
 
 
+def deg2num(lat_deg, lon_deg, zoom):
+  lat_rad = math.radians(lat_deg)
+  n = 1 << zoom
+  xtile = int((lon_deg + 180.0) / 360.0 * n)
+  ytile = int((1.0 - math.asinh(math.tan(lat_rad)) / math.pi) / 2.0 * n)
+  return xtile, ytile
 
-def getData(quadkeys):
+def num2deg(xtile, ytile, zoom):
+  n = 1 << zoom
+  lon_deg = xtile / n * 360.0 - 180.0
+  lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * ytile / n)))
+  lat_deg = math.degrees(lat_rad)
+  return lat_deg, lon_deg
+
+
+def getData(quadkeys, con):
     if not quadkeys:
         return []
     values_clause = ', '.join(f"('{quadkey}')" for quadkey in quadkeys)
@@ -22,47 +56,28 @@ def getData(quadkeys):
     JOIN quadkey_temp q
     ON t.quadkey = q.quadkey
     """
-    print(query)
     # Execute the query
-    return con.execute(query).fetchall()
+    result = con.execute(query).df()
+    result = result.fillna(0)
+    return result.to_dict(orient='records')
 
-def loadTileMale(z, y, x):
+def loadTileMale(z, y, x, con):
     qkey = quadkey.from_tile((x,y), z) # get quadtree for this tile
-    listofQKeys = qkey.children(z+3) # get all children 3 levels deeper -> 64 data points for this til
-    return getData(listofQKeys)
+    listofQKeys = qkey.children(z+1) # get all children 3 levels deeper -> 64 data points for this til
+    return getData(listofQKeys, con)
 
 
-# @asynccontextmanager
-# async def lifespan(app: FastAPI):
-#     """Run at startup
-#     Initialise the Client and add it to request.state
-#     """
-#     # Build the tree
-#     data_slice_male_quadkey = load_male_dataset()
-#     yield {"data_slice_male_quadkey": data_slice_male_quadkey}
-#     """ Run on shutdown
-#         Close the connection
-#         Clear variables and release the resources
-#     """
-#     # Cleanup the tree?
 
-# app = FastAPI(lifespan=lifespan)
 
-con = duckdb.connect("../../data/qudkeyDB.duckdb") # Note: duckdb.sql connects to the default in-memory database connection
-con.install_extension("spatial")
-con.load_extension("spatial")
+
 
 @app.get("/api/male/{z}/{y}/{x}")
-async def get_male_tile(z: int, y: int, x: int):
+async def get_male_tile(z: int, y: int, x: int, request: Request):
     # Call the function and return the result
-    result = loadTileMale(z, y, x)
+    result = loadTileMale(z, y, x, request.state.con)
     return result
 
-@app.get("/api/schema")
-async def showTables():
-    # Call the function and return the result
-    tables = con.execute("SHOW TABLES").fetchdf()
-    return tables
+
 
 # @app.get("/api/male/whole")
 # async def main(request: Request):
@@ -77,8 +92,8 @@ async def showTables():
 #     return df_to_json(filtered_data)
 
 
-# @app.get("/api/male/descendant/{quadkey}")
-# async def get_descendants(quadkey: int, request: Request):
-#     data_slice_male_quadkey = request.state.data_slice_male_quadkey
-#     filtered_data = filter_by_descendent(data_slice_male_quadkey, str(quadkey))
-#     return df_to_json(filtered_data)
+@app.get("/api/male/descendant/{quadkey}")
+async def get_descendants(quadkey: int, request: Request):
+    data_slice_male_quadkey = request.state.data_slice_male_quadkey
+    filtered_data = filter_by_descendent(data_slice_male_quadkey, str(quadkey))
+    return df_to_json(filtered_data)
